10ad735b2f6c05bdb2deb47366921120f73ab267
John Brewer <jbrewer@jera.com>
2013-11-25 20:05:57 -0800

    Detect left shoulder button pressed.

--- GameControllerTest/ViewController.m
+++ GameControllerTest/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <GameController/GameController.h>
 
-@interface ViewController ()
+@interface ViewController () {
+    GCController *_controller;
+    GCGamepad *_gamepad;
+    GCExtendedGamepad *_extendedGamepad;
+}
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -17,13 +24,59 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(controllerConnected:)
+                                                 name:GCControllerDidConnectNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(controllerDisconnected:)
+                                                 name:GCControllerDidDisconnectNotification
+                                               object:nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)log:(NSString*)string
+{
+    self.textView.text = [NSMutableString stringWithFormat:@"%@%@\n",
+                          self.textView.text, string];
+    CGRect caretRect = [_textView caretRectForPosition:_textView.endOfDocument];
+    [_textView scrollRectToVisible:caretRect animated:NO];
+}
+
+- (void)listControllers
+{
+    NSArray *controllers = [GCController controllers];
+    [self log:[NSString stringWithFormat:@"controllers = %@", controllers]];
+    for (GCController *controller in controllers) {
+        [self log:controller.description];
+    }
+}
+
+- (void)controllerConnected:(NSNotification*)notification
+{
+    GCController *controller = notification.object;
+    _controller = controller;
+    [self log:[NSString stringWithFormat:@"vendorName: %@", _controller.vendorName]];
+    [self log:[NSString stringWithFormat:@"playerIndex: %ld", (long)_controller.playerIndex]];
+    if (_controller.attachedToDevice) {
+        [self log:@"attachedToDevice"];
+    }
+    _controller.gamepad.leftShoulder.valueChangedHandler = ^(GCControllerButtonInput *button, float value, BOOL pressed) {
+        [self log:[NSString stringWithFormat:@"leftShoulder pressed = %d, value = %f",
+                   pressed, value]];
+    };
+}
+
+
+- (void)controllerDisconnected:(NSNotification*)notification
+{
+    GCController *controller = notification.object;
+    [self log:[NSString stringWithFormat:@"Controller %@ disconnected", controller]];
 }
 
 @end
